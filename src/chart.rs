e23f6d3a90cc14fb8b7bd0fc6d630987333445fd
morganamilo <morganamilo@archlinux.org>
2019-12-23 23:58:29 +0000

    Remove unneeded main in doc test

--- src/chart.rs
+++ src/chart.rs
@@ -8,19 +8,17 @@ use std::{convert::TryInto, io};
 /// ```rust
 /// use piechart::{Chart, Color, Data};
 ///
-/// fn main() {
-///     let data = vec![
-///         Data { label: "dd1".into(), value: 4.0, color: Some(Color::Red), fill: '•' },
-///         Data { label: "dd2".into(), value: 2.0, color: Some(Color::Green), fill: '•' },
-///         Data { label: "dd3".into(), value: 2.6, color: Some(Color::Blue), fill: '•' },
-///     ];
+/// let data = vec![
+///     Data { label: "dd1".into(), value: 4.0, color: Some(Color::Red), fill: '•' },
+///     Data { label: "dd2".into(), value: 2.0, color: Some(Color::Green), fill: '•' },
+///     Data { label: "dd3".into(), value: 2.6, color: Some(Color::Blue), fill: '•' },
+/// ];
 ///
-///     Chart::new()
-///         .radius(9)
-///         .aspect_ratio(2)
-///         .legend(true)
-///         .draw(&data);
-/// }
+/// Chart::new()
+///     .radius(9)
+///     .aspect_ratio(2)
+///     .legend(true)
+///     .draw(&data);
 /// ```
 /// will result in
 ///
